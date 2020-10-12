ab5abd944e810685cc78aa2b6fb5b4d64aeb3866
KaoCG <pumpkinowlteam@gmail.com>
2020-10-13 03:13:34 +0800

    Update CenterFunction.js

--- Script/CenterFunction.js
+++ Script/CenterFunction.js
@@ -150,7 +150,7 @@ async function GoToNextScene() {
   await centerComponent.currentStage++;
 
   //centerComponent.fadeUI.visible = false;
-  centerComponent.currentStage = 9;
+  //centerComponent.currentStage = 9;
   //loadScript("Script/SetScene3.js");
   //return;
 
