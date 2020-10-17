2b39845912193a7eb4ac153cbee22d21b8a5c689
KaoCG <pumpkinowlteam@gmail.com>
2020-10-17 22:52:18 +0800

    1017

--- Script/SetScene3.js
+++ Script/SetScene3.js
@@ -1527,8 +1527,8 @@ async function SetObject() {
       selectBoxes[0].addListener("pointerdown", () => { buttonA(); });
       selectBoxes[1].addListener("pointerdown", () => {
         PIXI.sound.play('button_click');
-
-        window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftncmmm1921.vercel.app%2F&amp;src=sdkpreparse');
+        window.location.replace("https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftncmmm1921.vercel.app%2F&amp;src=sdkpreparse");
+        //window.open('https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftncmmm1921.vercel.app%2F&amp;src=sdkpreparse');
       });
       selectBoxes[2].addListener("pointerdown", () => { buttonC(); });
       selectBoxes[3].addListener("pointerdown", () => { buttonD(); });
